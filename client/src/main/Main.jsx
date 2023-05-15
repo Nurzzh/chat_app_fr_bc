c3f79ff0f95377650e9a96e2fa8394336c4df752
nurzzh <nurzzh@yandex.ru>
2023-05-15 17:35:38 +0600

    main
    main

---
+++ client/src/main/Main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Logo from './twitter-logo.png'
+import './Main.css'
+
+const Main = () => {
+  return (
+    <div className='container'>
+        <div className='main_header'>
+            <img className='logo_twitter' src={Logo} alt="img" />
+            <input className='header_input' type="text" placeholder='# Explore' />
+            <button className='home_button'>HOME</button>
+            <button className='msg_button'>MSG</button>
+            <button className='msg_button'>NTFCN</button>
+        </div>
+        <div className='main_content'>
+            <div className='content_left'></div>
+            <div className='content_right'></div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
--- client/src/router/Router.jsx
+++ client/src/router/Router.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route} from 'react-router-dom'
 import { Navigate, Routes} from 'react-router'
 import Login from '../login/login'
 import Register from '../register/Register'
+import Main from '../main/Main'
 
 
 const Router = () =>{
@@ -11,6 +12,7 @@ const Router = () =>{
                 <Route path="/" element={<Navigate to ='/login'/>} />
                 <Route path='/login' element={<Login />}/>
                 <Route path='/register' element={<Register />}/>
+                <Route path='/main' element={<Main/>}/>
             </Routes>
         </BrowserRouter>
     )
